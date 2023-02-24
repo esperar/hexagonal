51a6174fa434bd8eaea4252a6b0d057816572ea2
aesperer <piyrw9754@gmail.com>
2023-02-24 17:02:05 +0900

    :recycle: 모듈 이름 변경
    모듈 이름 변경

--- hexagonal-configuration/src/main/java/esperer/ConfigurationApplication.java
+++ hexagonal-configuration/src/main/java/esperer/ConfigurationApplication.java
@@ -1,6 +1,7 @@
 package esperer;
 
-public class Main {
+@SpringBootApplication
+public class ConfigurationApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
